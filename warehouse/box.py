30fad6daabd9991e77dc75fc06403c47d6445c8f
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-06-30 10:51:34 +0200

    Add docstring for Box

--- warehouse/box.py
+++ warehouse/box.py
@@ -1,4 +1,8 @@
 class Box:
+    """
+    Represent a box in the warehouse
+    """
+
     def __init__(self, content):
         self.content = list(content)
         self.letters = set(content)
@@ -6,10 +10,16 @@ class Box:
 
     @property
     def has_double(self):
+        """
+        Return True if the box have two letters at least once
+        """
         if 2 in self.count.values():
             return True
 
     @property
     def has_triple(self):
+        """
+        Return True if the box have three letters at least once
+        """
         if 3 in self.count.values():
             return True
