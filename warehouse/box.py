be6ac9d4a83b879789887fcde40cb58b822941b4
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-06-30 11:48:43 +0200

    Change the name of content for box_id

--- warehouse/box.py
+++ warehouse/box.py
@@ -3,23 +3,25 @@ class Box:
     Represent a box in the warehouse
     """
 
-    def __init__(self, content):
-        self.content = list(content)
-        self.letters = set(content)
-        self.count = {letter: content.count(letter) for letter in self.letters}
+    def __init__(self, box_id):
+        self.box_id = list(box_id)
+        self.letters = set(box_id)
+        self.count = {letter: box_id.count(letter) for letter in self.letters}
 
     @property
     def has_double(self):
         """
-        Return True if the box have two letters at least once
+        Return True if the box_id have two letters at least once
         """
         if 2 in self.count.values():
             return True
+        return False
 
     @property
     def has_triple(self):
         """
-        Return True if the box have three letters at least once
+        Return True if the box_id have three letters at least once
         """
         if 3 in self.count.values():
             return True
+        return False
