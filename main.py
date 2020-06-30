a1175dc2c38dbe03808749cefcc2b30953631a3f
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-06-30 16:40:32 +0200

    Find the two correct boxes and common letters between their IDs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from warehouse.box import Box
 from warehouse.stock import Stock
+from warehouse.finder import Finder
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -46,10 +47,31 @@ class App:
         """
         return self.stock.checksum
 
+    @property
+    def common_letters_between_correct_boxes(self):
+        """
+        Find the two correct boxes and return the common letters between their ID
+        """
+        boxes_to_compare = list(self.boxes)
+        boxes_common_letters = str()
+        finder = Finder()
+
+        for box1 in self.boxes:
+            for box2 in boxes_to_compare:
+                if finder.compare_boxes(box1, box2):
+                    # find common letters between the two correct boxes
+                    boxes_common_letters = finder.common_letters(box1, box2)
+                    break
+            if boxes_common_letters:
+                break
+
+        return boxes_common_letters
+
 
 if __name__ == "__main__":
     app = App()
     app.open_file()
     app.read_boxes()
     app.store_boxes()
-    print(app.stock_checksum)
+    print(f"Answer for Part I  📝 {app.stock_checksum}")
+    print(f"Answer for Part II 📦 {app.common_letters_between_correct_boxes}")
