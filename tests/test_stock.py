dee3885da27feb3ebb3f97eee8b6efe7907b8da7
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-06-30 11:06:20 +0200

    Split test_warehouse in test_box and test_stock

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -1,37 +1,4 @@
-from warehouse.box import Box
 from warehouse.stock import Stock
-
-
-class TestBox:
-    def test_box_has_no_double_and_no_triple_letters(self):
-        box = Box("abcdef")
-        assert not box.has_double
-        assert not box.has_triple
-
-    def test_box_has_double_letters_once_and_triple_letters_once(self):
-        box = Box("bababc")
-        assert box.has_double
-        assert box.has_triple
-
-    def test_box_has_double_letters_once_and_no_triple_letters(self):
-        box = Box("abbcde")
-        assert box.has_double
-        assert not box.has_triple
-
-    def test_box_has_no_double_letters_and_triple_letters_once(self):
-        box = Box("abcccd")
-        assert not box.has_double
-        assert box.has_triple
-
-    def test_box_has_double_letters_twice_and_no_triple_letters(self):
-        box = Box("aabcdd")
-        assert box.has_double
-        assert not box.has_triple
-
-    def test_box_has_no_double_letters_and_triple_letters_twice(self):
-        box = Box("ababab")
-        assert not box.has_double
-        assert box.has_triple
 
 
 class MockStockAddBox:
