50347ca1d2e999b167690823301b057c1b9e6d0f
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-06-30 10:51:50 +0200

    Add docstring for Stockw

--- warehouse/stock.py
+++ warehouse/stock.py
@@ -1,16 +1,29 @@
 class Stock:
+    """
+    Represent the warehouse
+    """
+
     def __init__(self):
         self._boxes = list()
 
     def add_box(self, box):
+        """
+        Store a box in the warehouse
+        """
         self._boxes.append(box.content)
 
     @property
     def two_letters_boxes(self):
+        """
+        Count boxes with two letters inside
+        """
         return self._count_boxes(2)
 
     @property
     def three_letters_boxes(self):
+        """
+        Count boxes with three letters inside
+        """
         return self._count_boxes(3)
 
     @property
@@ -18,6 +31,9 @@ class Stock:
         return self.two_letters_boxes * self.three_letters_boxes
 
     def _count_boxes(self, letters_number):
+        """
+        Count boxes with letters_number inside
+        """
         boxes_number = 0
         for box in self._boxes:
             letters = set(box)
